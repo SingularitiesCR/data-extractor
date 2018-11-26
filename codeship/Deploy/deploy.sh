c969f2962057f1ada7b03eef15bba35c53509f93
José Marín <jomarinb@users.noreply.github.com>
2018-11-26 11:14:09 -0600

    codeship: fix copy of src dir

--- codeship/Deploy/deploy.sh
+++ codeship/Deploy/deploy.sh
@@ -4,5 +4,5 @@ readonly CI_COMMIT_ID=${CI_COMMIT_ID}
 
 mvn -q versions:set -DnewVersion=${CI_COMMIT_ID} && \
     mvn -q versions:commit && \
-    mvn -q deploy && \
+    mvn -q deploy -Dmaven.test.skip=true && \
     echo "deployed service-apis version ${CI_COMMIT_ID}"
