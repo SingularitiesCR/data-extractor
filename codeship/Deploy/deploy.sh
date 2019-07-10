0153c96bc406b80547ceb21a07ac93377775dd54
Jose Marin <jomarinb@users.noreply.github.com>
2019-07-10 14:46:38 -0600

    codeship: update deploy service

--- codeship/Deploy/deploy.sh
+++ codeship/Deploy/deploy.sh
@@ -2,7 +2,6 @@
 
 readonly CI_COMMIT_ID=${CI_COMMIT_ID}
 
-mvn -q versions:set -DnewVersion=${CI_COMMIT_ID} && \
-    mvn -q versions:commit && \
-    mvn -q deploy -Dmaven.test.skip=true && \
-    echo "deployed service-apis version ${CI_COMMIT_ID}"
+mvn versions:set -q -DnewVersion=${CI_COMMIT_ID} && \
+    mvn versions:commit -q && \
+    mvn deploy -q -Dmaven.test.skip=true
